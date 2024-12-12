31dd3265e9b207566a025847f8f3b9784bda8b5b
idewbayu14 <idewanyomanbayusw@gmail.com>
2024-12-12 14:37:13 +0700

    update

--- app/api/[id]/route.ts
+++ app/api/[id]/route.ts
@@ -1,7 +1,7 @@
 import { db } from "@/lib/db"
 import { NextResponse } from "next/server"
 
-export async function GET(req: Request, { params }: { params: { id: string } }) {
+export async function GET( { req, params }: { params: { id: string }, req: Request }) {
   try {
     const { id } = params
     const computer = await db.computer.findUnique({
