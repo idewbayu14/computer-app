a6be3d2d6e5b9c1085f15c4e76dfa2e0811621c9
idewbayu14 <idewanyomanbayusw@gmail.com>
2024-12-13 16:33:08 +0700

    test api

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,6 +4,7 @@ import { db } from "@/lib/db"
 import { ContentComponent } from "./_components/content"
 
 const LandingPage = async () => {
+  
   const dataComputer = await db.computer.findMany() 
 
   return (
--- app/api/computer/route.ts
+++ app/api/computer/route.ts
@@ -1,6 +1,18 @@
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
+
+export async function GET(req: Request) {
+    try{
+
+        const computer = await db.computer.findMany()
+
+        return NextResponse.json(computer)
+    }catch(error){
+        return NextResponse.json({message: "something went wrong", status: 200})
+    }
+}
+
 export async function POST(req: Request) {
     try {
         const body = await req.json();
